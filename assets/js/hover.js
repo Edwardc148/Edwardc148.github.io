980db586c03ae22b21fafbb80c75e50ca5b109e5
Edward <edwardcheng148@gmail.com>
2018-03-08 23:57:52 -0800

    added analytics

---
+++ assets/js/hover.js
@@ -0,0 +1,16 @@
+let projects = document.getElementsByClassName('proj');
+
+for (let i = 0; i < projects.length; i++) {
+  projects[i].addEventListener('mouseenter', (e) => {
+    // console.log(projects[i]);
+    e.target.getElementsByClassName('pic-view')[0].style += ";display:block";
+    e.target.getElementsByClassName('view-proj')[0].style += ";display:block";
+    e.target.style += ";background: rgba(0,0,0,0.8)";
+  });
+
+  projects[i].addEventListener('mouseleave', (e) => {
+    e.target.getElementsByClassName('pic-view')[0].style -= ";display:block;";
+    e.target.getElementsByClassName('view-proj')[0].style -= ";display:block";
+    e.target.style -= ";background: rgba(0,0,0,0.8)";
+  });
+}
