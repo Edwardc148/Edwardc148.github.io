e39631dcfde86b57e5aad96f68718ced5ba6cbca
Edward <edwardcheng148@gmail.com>
2018-03-10 21:12:34 -0800

    added changes to styling in personal portfolio

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -4,13 +4,13 @@ for (let i = 0; i < projects.length; i++) {
   projects[i].addEventListener('mouseenter', (e) => {
     // console.log(projects[i]);
     e.target.getElementsByClassName('pic-view')[0].style += ";display:block";
-    e.target.getElementsByClassName('view-proj')[0].style += ";display:block";
+    // e.target.getElementsByClassName('view-proj')[0].style += ";display:block";
     e.target.style += ";background: rgba(0,0,0,0.8)";
   });
 
   projects[i].addEventListener('mouseleave', (e) => {
     e.target.getElementsByClassName('pic-view')[0].style -= ";display:block;";
-    e.target.getElementsByClassName('view-proj')[0].style -= ";display:block";
+    // e.target.getElementsByClassName('view-proj')[0].style -= ";display:block";
     e.target.style -= ";background: rgba(0,0,0,0.8)";
   });
 }
